04674973fc63d4f473e35d75cc8f11757ce7cf2b
xaviguasch <xaviguasch@gmail.com>
2020-05-01 23:08:02 +0200

    hidden all panels on click

--- tabs.js
+++ tabs.js
@@ -3,7 +3,14 @@ const tabButtons = tabs.querySelectorAll('[role="tab"]')
 const tabPanels = tabs.querySelectorAll('[role="tabpanel"]')
 
 function handleTabClick(event) {
-  console.log(event)
+  // hide all tab panels
+  tabPanels.forEach((panel) => (panel.hidden = true))
+
+  // mark all tabs as unselected
+
+  // mark the clicked tab as selected
+
+  // find the associated tabPanel and show it!
 }
 
 tabButtons.forEach((button) => button.addEventListener('click', handleTabClick))
