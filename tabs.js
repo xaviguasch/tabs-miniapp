c8684ebabb971a1903d4550c5b27e0809202aa65
xaviguasch <xaviguasch@gmail.com>
2020-05-01 22:47:21 +0200

    added selectors and handleClickTab function

--- tabs.js
+++ tabs.js
@@ -1,1 +1,9 @@
-console.log('ya ya wes we get it.. IT WORKS!');
+const tabs = document.querySelector('.tabs')
+const tabButtons = tabs.querySelectorAll('[role="tab"]')
+const tabPanels = tabs.querySelectorAll('[role="tabpanel"]')
+
+function handleTabClick(event) {
+  console.log(event)
+}
+
+tabButtons.forEach((button) => button.addEventListener('click', handleTabClick))
